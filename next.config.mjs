523b97af7c16b62c2e78e8722429339b0323d5c5
Chinmay-innovates <chinmaygope69@gmail.com>
2024-04-19 04:31:24 +0530

    deploying

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
